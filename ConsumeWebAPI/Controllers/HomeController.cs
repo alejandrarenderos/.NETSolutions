b879996cdbf1c4f2b67d5ba7dea1da475e93b266
alejandrarenderos <dannielarenderos@gmail.com>
2022-10-07 01:06:07 -0600

    first commit

---
+++ ConsumeWebAPI/Controllers/HomeController.cs
@@ -0,0 +1,72 @@
+﻿using ConsumeWebAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace ConsumeWebAPI.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        string baseUrl = "https://jsonplaceholder.typicode.com/";
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            DataTable dt = new DataTable();
+            List<Photos> modelList = new List<Photos>(); 
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(baseUrl);
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+
+
+                HttpResponseMessage getData = await client.GetAsync("photos");
+
+                if (!getData.IsSuccessStatusCode)
+                {
+                    Console.WriteLine("Error while calling API");
+                }
+                else
+                {
+                    String results = getData.Content.ReadAsStringAsync().Result;
+                    modelList = JsonConvert.DeserializeObject<List<Photos>>(results);
+                    Console.WriteLine(results);
+
+                }
+
+
+            }
+
+                return View(modelList);
+
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
